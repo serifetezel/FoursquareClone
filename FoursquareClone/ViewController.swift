3b9dbe9be96d3f15d4da75eebffa8c2b71e6e141
Şerife Tezel <serife.tezel@vomsis.com>
2023-10-03 11:46:53 +0300

    Initial Commit

---
+++ FoursquareClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by Fatih Aydoğdu on 3.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
